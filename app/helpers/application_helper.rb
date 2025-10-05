da3d9d0d75fd1b415134e2f68f5b7889f5fcc0f1
m-h09 <ermynb0srstgojpn26@gmail.com>
2025-10-05 18:17:07 +0900

    org修正２

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,20 +7,20 @@ module ApplicationHelper
       charset: "utf-8",
       description: "テキストを関西弁に変換し、絵文字を追加します。",
       canonical: request.original_url,
-      separatir: "|",
+      separator: "|",
       og: {
         site_name: "EMOJI関西弁変換",
         title: "EMOJI関西弁変換",
         description: "テキストを関西弁に変換し、絵文字を追加します。",
         type: "website",
         url: request.original_url,
-        image: image_url("/ogp/EMOJI.png"),
+        image: asset_url("/ogp/EMOJI.png"),
         locale: "ja_JP"
       },
       twitter: {
         card: "summary_large_image",
-        site: "@https://x.com/rnteq_64_07",
-        image: image_url("/ogp/EMOJI.png")
+        site: "@rnteq_64_07",
+        image: asset_url("/ogp/EMOJI.png")
       }
     }
   end
--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
+    config.assets.precompile += %w[ogp/EMOJI.png]
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
