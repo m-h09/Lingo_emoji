2b6c499efd3107832946b3091b328654f525f718
m-h09 <ermynb0srstgojpn26@gmail.com>
2025-10-06 00:02:49 +0900

    アイキャッチ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-require "meta-tags"
-
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   include MetaTags::ControllerHelper
