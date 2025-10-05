9e7db0a3482c8d019c1af03eda0f5aed7a99a278
m-h09 <ermynb0srstgojpn26@gmail.com>
2025-10-05 12:56:59 +0900

    lintエラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-require 'meta-tags'
+require "meta-tags"
 
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,26 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'EMOJI関西弁変換',
-      title: 'EMOJI関西弁変換',
+      site: "EMOJI関西弁変換",
+      title: "EMOJI関西弁変換",
       reverse: true,
-      charset: 'utf-8',
-      description: 'テキストを関西弁に変換し、絵文字を追加します。',
+      charset: "utf-8",
+      description: "テキストを関西弁に変換し、絵文字を追加します。",
       canonical: request.original_url,
-      separatir: '|',
+      separatir: "|",
       og: {
-        site_name: 'EMOJI関西弁変換',
-        title: 'EMOJI関西弁変換',
-        description: 'テキストを関西弁に変換し、絵文字を追加します。',
-        type: 'website',
+        site_name: "EMOJI関西弁変換",
+        title: "EMOJI関西弁変換",
+        description: "テキストを関西弁に変換し、絵文字を追加します。",
+        type: "website",
         url: request.original_url,
-        image: image_url('/ogp/EMOJI.png'),
-        locale: 'ja_JP'
+        image: image_url("/ogp/EMOJI.png"),
+        locale: "ja_JP"
       },
       twitter: {
-        card: 'summary_large_image',
-        site: '@https://x.com/rnteq_64_07',
-        image: image_url('/ogp/EMOJI.png')
+        card: "summary_large_image",
+        site: "@https://x.com/rnteq_64_07",
+        image: image_url("/ogp/EMOJI.png")
       }
     }
   end
