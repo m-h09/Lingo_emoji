e122684c0678de2412bc97a8053b7ac150d2c299
m-h09 <ermynb0srstgojpn26@gmail.com>
2025-11-01 00:38:21 +0900

    アドレスの直書きを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
-  config.action_mailer.default_options = { from: "music8.d0fss2@gmail.com" }
+  config.action_mailer.default_options = { from: ENV["MAIL_SENDER_ADDRESS"] }
 end
