05813b82dad64c65a8ffc08062fca5c4bf5049bc
m-h09 <ermynb0srstgojpn26@gmail.com>
2025-11-16 01:20:01 +0900

    表示が崩れたため修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
+# pin "application"
+# pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+# pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.18
+# pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.18
 pin "application"
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.18
-pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.18
+pin "@hotwired/turbo-rails"
