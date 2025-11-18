eda6b078318e8ced15f2898274e4af14e0c07727
m-h09 <ermynb0srstgojpn26@gmail.com>
2025-11-18 11:00:57 +0900

    微調整

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get "main/edit_history", to: "main#edit_history"
   post "main/add_history", to: "main#add_history", as: :main_add_history
   post "main/delete_history", to: "main#delete_history", as: :main_delete_history
+  post "main/translation_delete", to: "main#translation_delete", as: :main_translation_delete
   get "templates", to: "main#templates"
   get "guide", to: "main#guide"
   root "main#index"
