94c2f8376c0a0b62b01da2c30beef4cacc82f2b4
m-h09 <ermynb0srstgojpn26@gmail.com>
2025-12-21 12:36:18 +0900

    CSSタイトル修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.hosts << "192.168.10.7"
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
